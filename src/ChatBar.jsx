76b15252835d6229fb702eb7f0a95795e1b551a6
Linley Faulkner <linleyjfaulkner@gmail.com>
2016-11-02 04:08:01 +0000

    Handing User Actions

--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,11 +2,30 @@ import React, {Component} from 'react';
 
 
 class ChatBar extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {newMessage: 'gfh'};
+    this.handleChange = this.handleChange.bind(this);
+
+  }
+
+  handleChange(event) {
+      this.setState({newMessage: event.target.value});
+  }
+
   render() {
     return (
       <footer>
-          <input id="username" type="text" placeholder="Your Name (Optional)" value={this.props.currentUser} />
-          <input id="new-message" type="text" placeholder="Type a message and hit ENTER" />
+          <input id="username"
+            type="text" placeholder="Your Name (Optional)"
+            value={this.props.currentUser} />
+          <input
+            id="new-message"
+            type="text"
+            placeholder="Type a message and hit ENTER"
+            value={this.state.newMessage}
+            onChange={this.handleChange} />
       </footer>
     );
   }
