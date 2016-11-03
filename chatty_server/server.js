eedd449f22e8259fc5531b74fc251d05cc6e65f1
Linley Faulkner <linleyjfaulkner@gmail.com>
2016-11-03 00:13:47 +0000

    adding unique ID

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
+const uuid = require('node-uuid');
 
 // Set the port to 4000
 const PORT = 4000;
@@ -15,6 +16,14 @@ const server = express()
 // Create the WebSockets server
 const wss = new SocketServer({ server });
 
+
+wss.broadcast = function broadcast(outgoingMessage) {
+  wss.clients.forEach(function each(client) {
+    client.send(outgoingMessage);
+  });
+  console.log("sending" + outgoingMessage)
+};
+
 // Set up a callback that will run when a client connects to the server
 // When a client connects they are assigned a socket, represented by
 // the ws parameter in the callback.
@@ -23,6 +32,14 @@ wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
   var parsedMessage = JSON.parse(message)
   console.log(parsedMessage);
+  let uniqueid = uuid.v1();
+  let outgoingMessage = {
+    id: uniqueid,
+    username: parsedMessage.username,
+    content: parsedMessage.content
+  }
+  wss.broadcast(JSON.stringify(outgoingMessage));
+
   });
   // Set up a callback for when a client closes the socket. This usually means they closed their browser.
   ws.on('close', () => console.log('Client disconnected'));
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,18 +10,7 @@ class App extends Component {
     this.addMessage=this.addMessage.bind(this);
       this.state = {
         currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-        messages: [
-          {
-            id: 1,
-            username: "Bob",
-            content: "Has anyone seen my marbles?",
-          },
-          {
-            id: 2,
-            username: "Anonymous",
-            content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-          }
-        ],
+        messages: [],
         socket: {}
       }
   }
